8abb6111ebdc83c293fade108da80ff45bd26bfc
Baradoy <graham.baradoy@gmail.com>
2019-02-17 22:22:29 -0800

    Added monsters images and definitions. I will need to split up monsters and player charcaters

--- assets/js/definitions.js
+++ assets/js/definitions.js
@@ -51,5 +51,163 @@ export const allCharacters = [
     image: "/images/classes/windows.jpg",
     name: "windows",
     type: "character"
+  },
+  {
+    image: "/images/monsters/Ancient-Artillery.jpg",
+    name: "Ancient Artillery",
+    type: "monster"
+  },
+  {
+    image: "/images/monsters/Bandit-Archer.jpg",
+    name: "Bandit Archer",
+    type: "monster"
+  },
+  {
+    image: "/images/monsters/Bandit-Guard.jpg",
+    name: "Bandit Guard",
+    type: "monster"
+  },
+  {
+    image: "/images/monsters/Black-Imp.jpg",
+    name: "Black Imp",
+    type: "monster"
+  },
+  {
+    image: "/images/monsters/Cave-Bear.jpg",
+    name: "Cave Bear",
+    type: "monster"
+  },
+  {
+    image: "/images/monsters/City-Archer.jpg",
+    name: "City Archer",
+    type: "monster"
+  },
+  {
+    image: "/images/monsters/City-Guard.jpg",
+    name: "City Guard",
+    type: "monster"
+  },
+  {
+    image: "/images/monsters/Cultist.jpg",
+    name: "Cultist",
+    type: "monster"
+  },
+  {
+    image: "/images/monsters/Deep-Terror.jpg",
+    name: "Deep Terror",
+    type: "monster"
+  },
+  {
+    image: "/images/monsters/Earth-Demon.jpg",
+    name: "Earth Demon",
+    type: "monster"
+  },
+  {
+    image: "/images/monsters/Flame-Demon.jpg",
+    name: "Flame Demon",
+    type: "monster"
+  },
+  {
+    image: "/images/monsters/Forest-Imp.jpg",
+    name: "Forest Imp",
+    type: "monster"
+  },
+  {
+    image: "/images/monsters/Frost-Demon.jpg",
+    name: "Frost Demon",
+    type: "monster"
+  },
+  {
+    image: "/images/monsters/Giant-Viper.jpg",
+    name: "Giant Viper",
+    type: "monster"
+  },
+  {
+    image: "/images/monsters/Harrower-Infester.jpg",
+    name: "Harrower Infester",
+    type: "monster"
+  },
+  { image: "/images/monsters/Hound.jpg", name: "Hound.jpg", type: "monster" },
+  {
+    image: "/images/monsters/Inox-Archer.jpg",
+    name: "Inox Archer",
+    type: "monster"
+  },
+  {
+    image: "/images/monsters/Inox-Guard.jpg",
+    name: "Inox Guard",
+    type: "monster"
+  },
+  {
+    image: "/images/monsters/Inox-Shaman.jpg",
+    name: "Inox Shaman",
+    type: "monster"
+  },
+  {
+    image: "/images/monsters/LivingBOnes.jpg",
+    name: "LivingBOnes",
+    type: "monster"
+  },
+  {
+    image: "/images/monsters/LivingSpririt.jpg",
+    name: "Living Spririt",
+    type: "monster"
+  },
+  { image: "/images/monsters/Lurker.jpg", name: "Lurker.jpg", type: "monster" },
+  {
+    image: "/images/monsters/Night-Demon.jpg",
+    name: "Night Demon",
+    type: "monster"
+  },
+  { image: "/images/monsters/Ooze.jpg", name: "Ooze.jpg", type: "monster" },
+  {
+    image: "/images/monsters/Savvas-Icestorm.jpg",
+    name: "Savvas Icestorm",
+    type: "monster"
+  },
+  {
+    image: "/images/monsters/Savvas-Lavaflow.jpg",
+    name: "Savvas Lavaflow",
+    type: "monster"
+  },
+  {
+    image: "/images/monsters/Living-Bones.jpg",
+    name: "Living Bones",
+    type: "monster"
+  },
+  {
+    image: "/images/monsters/Spitting-Drake.jpg",
+    name: "Spitting Drake",
+    type: "monster"
+  },
+  {
+    image: "/images/monsters/Stone-Golem.jpg",
+    name: "Stone Golem",
+    type: "monster"
+  },
+  {
+    image: "/images/monsters/Sun-Demon.jpg",
+    name: "Sun Demon",
+    type: "monster"
+  },
+  {
+    image: "/images/monsters/Vermling-Scout.jpg",
+    name: "Vermling Scout",
+    type: "monster"
+  },
+  {
+    image: "/images/monsters/Vermling-Shaman.jpg",
+    name: "Vermling Shaman",
+    type: "monster"
+  },
+  {
+    image: "/images/monsters/Vicious-Drake.jpg",
+    name: "Vicious Drake",
+    type: "monster"
+  },
+  {
+    image: "/images/monsters/Wind-Demon.jpg",
+    name: "Wind Demon",
+    type: "monster"
   }
 ];
