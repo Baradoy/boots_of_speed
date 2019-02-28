a6474b94409faa9b7607dec056b3bfb36aa2aece
Baradoy <graham.baradoy@gmail.com>
2019-02-28 13:58:19 -0800

    Added further Gigilixir config

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,12 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :boots_of_speed, BootsOfSpeedWeb.Endpoint,
-  http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "example.com", port: 80],
+  load_from_system_env: true,
+  http: [:inet6, port: {:system, "PORT"}],
+  # Without this line, your app will not start the web server!
+  server: true,
+  secret_key_base: "${SECRET_KEY_BASE}",
+  url: [host: "${APP_NAME}.gigalixirapp.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
