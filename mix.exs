f461527ca4c934bc63185fe4e087c75df936c040
Graham Baradoy <graham.baradoy@pixelunion.net>
2019-06-30 10:54:25 -0700

    Add Credo back in

--- mix.exs
+++ mix.exs
@@ -40,6 +40,7 @@ defmodule BootsOfSpeed.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
+      {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0.0-rc.6", only: [:dev, :test], runtime: false}
     ]
   end
